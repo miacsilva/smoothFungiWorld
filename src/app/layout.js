068aeb327ab61c5d79f5fdcb84fae6d5639c2c27
Maria Carvalho <miacsilvac@hotmail.com>
2024-03-12 17:53:22 +0000

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+    <link rel="icon" href="/images/favicon.png" sizes="any" />
       <body className={inter.className}>{children}</body>
     </html>
   )
